0b6a8fbf69167080bc2195e08615223663c20119
Winston Jr Rodriguez Stand <wijurost@gmail.com>
2018-02-15 10:30:08 -0500

    Agregando Autenticacion de Usuario

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "figaro"
 gem 'rspotify'
+
+# Para hacer autenticacion de usuario
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  #devise_for :users
   resources :playlists
   resources :tracks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tracks#index'
+  #devise_for :users do
+    #get '/users/sign_out' => 'devise/sessions#destroy'
+  #end
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 end
